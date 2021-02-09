e5fbc236edebe29cc72aa8ed253fa468fbce06bd
lucifer-999 <ayush.bajaj3210@gmail.com>
2021-02-09 14:39:50 +0530

    Initial commit

---
+++ rev/grep/flag.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+/*
+void base64decode ( char* encoded );
+
+int main() {
+	char decoded[100] = "e3V1eXwnY0e3V1eXwnY0ZjMEfndoaCsDbnWBBXY/BA9hSjZURmE2SH8=";
+	char xor[100] = "06624f4da489e774a32292a235373b38";
+
+	char decoded[100];
+	int i;
+
+	base64decode(decoded);
+
+	for ( i = 0; i < strlen(decoded) ; i++ ) {
+		decode[i] = decode[i] - 2;
+		decode[i] = decode[i] ^ xor[i];
+
+		printf( "%c", decode[i] );
+	}
+}
+*/
+
+int main() {
+	printf ("IEEENCU.CTF{GR3PP1NG_TH3_FL4G_15_H4RD_4F}");
+	return 0;
+}
